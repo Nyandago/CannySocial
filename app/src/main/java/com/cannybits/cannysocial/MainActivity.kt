e460553b08db72d8a5a7262b1047496192456931
nyandago <paulmutator@gmail.com>
2021-09-21 18:55:04 +0300

    Login button activity

--- app/src/main/java/com/cannybits/cannysocial/MainActivity.kt
+++ app/src/main/java/com/cannybits/cannysocial/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.cannybits.cannysocial
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var btnLogIn : Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initView()
+
+        btnLogIn.setOnClickListener {
+            val intent = Intent(this@MainActivity, LoginActivity::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+
+    private fun initView(){
+        btnLogIn = btnSignIn
     }
 }
