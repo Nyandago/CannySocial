2af4a0758f2c1a38ca23b2302e758b5207b87e14
nyandago <paulmutator@gmail.com>
2021-09-23 14:19:41 +0300

    Sign Up fragment

--- app/src/main/java/com/cannybits/cannysocial/WelcomeFragment.kt
+++ app/src/main/java/com/cannybits/cannysocial/WelcomeFragment.kt
@@ -18,11 +18,11 @@ class WelcomeFragment : Fragment() {
         // Inflate the layout for this fragment
       val view = inflater.inflate(R.layout.fragment_welcome, container, false)
 
-        view.btnSignIn.setOnClickListener {
+        view.btnToSignInPage.setOnClickListener {
             Navigation.findNavController(view).navigate(R.id.action_welcomeFragment_to_loginFragment)
         }
 
-        view.btnSignUp.setOnClickListener {
+        view.btnToSignUpPage.setOnClickListener {
             Navigation.findNavController(view).navigate(R.id.action_welcomeFragment_to_signUpFragment)
         }
 
