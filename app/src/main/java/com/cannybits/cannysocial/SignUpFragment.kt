079b007a1ff0a2967e3e3c23e55230744eab2510
nyandago <paulmutator@gmail.com>
2021-09-24 15:26:28 +0300

    Sign Up Fragment

--- app/src/main/java/com/cannybits/cannysocial/SignUpFragment.kt
+++ app/src/main/java/com/cannybits/cannysocial/SignUpFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.navigation.Navigation
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.fragment_sign_up.*
@@ -50,10 +51,9 @@ class SignUpFragment : Fragment() {
                 .addOnCompleteListener() {
                     task ->
                     if(task.isSuccessful){
-                        val user = task.result!!.user
-                       Log.d("User","Success: Added user ="+user.toString())
+                       Toast.makeText(activity,"User Successfully Added",Toast.LENGTH_LONG).show()
                     } else {
-                        Log.d("User","Add Failed: $task")
+                        Toast.makeText(activity,"Failed To Add The User",Toast.LENGTH_LONG).show()
                     }
                 }
         }
