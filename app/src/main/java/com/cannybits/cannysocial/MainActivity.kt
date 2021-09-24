cb9eef5296857302b7a5047971de3c6a52d69ef1
nyandago <paulmutator@gmail.com>
2021-09-24 15:25:54 +0300

    Main Activity

--- app/src/main/java/com/cannybits/cannysocial/MainActivity.kt
+++ app/src/main/java/com/cannybits/cannysocial/MainActivity.kt
@@ -14,13 +14,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        initView()
-
-
-
     }
 
-    private fun initView(){
 
-    }
 }
