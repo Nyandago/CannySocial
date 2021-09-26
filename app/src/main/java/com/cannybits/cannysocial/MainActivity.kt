e549376eecd40d669b46be792215e46c6ddb6dc6
nyandago <paulmutator@gmail.com>
2021-09-27 02:31:54 +0300

    Main Activity

--- app/src/main/java/com/cannybits/cannysocial/MainActivity.kt
+++ app/src/main/java/com/cannybits/cannysocial/MainActivity.kt
@@ -6,7 +6,15 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import com.google.android.gms.auth.api.signin.GoogleSignIn
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount
+import com.google.android.gms.auth.api.signin.GoogleSignInClient
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions
+import com.google.android.gms.common.api.ApiException
+import com.google.android.gms.tasks.Task
+import com.google.firebase.FirebaseApp
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.GoogleAuthProvider
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -15,7 +23,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var signIn: Button
     private lateinit var googleSignIn: Button
     private lateinit var createAccount: Button
-    private var auth: FirebaseAuth? = null
+    private lateinit var firebaseAuth: FirebaseAuth
+
+    lateinit var mGoogleSignInClient: GoogleSignInClient
+    val Req_Code:Int=123
+
+
 
 
 
@@ -24,7 +37,16 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         initView()
-        auth = FirebaseAuth.getInstance()
+
+        FirebaseApp.initializeApp(this)
+
+        val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+            .requestIdToken(getString(R.string.default_web_client_id))
+            .requestEmail()
+            .build()
+        mGoogleSignInClient= GoogleSignIn.getClient(this,gso)
+
+        firebaseAuth = FirebaseAuth.getInstance()
 
         createAccount.setOnClickListener {
             Intent(this, SignUpActivity::class.java).also {
@@ -45,7 +67,7 @@ class MainActivity : AppCompatActivity() {
             Intent(this, UserProfileActivity::class.java).also {
                 startActivity(it)
             }
-            clearScreen()
+            clearEditText()
         }
 
     }
@@ -59,11 +81,53 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun signInWithGoogle(){
-        //google Sign In
+        val signInIntent: Intent =mGoogleSignInClient.signInIntent
+        startActivityForResult(signInIntent,Req_Code)
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(requestCode==Req_Code){
+            val task: Task<GoogleSignInAccount> = GoogleSignIn.getSignedInAccountFromIntent(data)
+            handleResult(task)
+//            firebaseAuthWithGoogle(account!!)
+        }
+    }
+
+    private fun handleResult(completedTask: Task<GoogleSignInAccount>){
+        try {
+            val account: GoogleSignInAccount? =completedTask.getResult(ApiException::class.java)
+            if (account != null) {
+                UpdateUI(account)
+            }
+        } catch (e: ApiException){
+            Toast.makeText(this,e.toString(), Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private fun UpdateUI(account: GoogleSignInAccount){
+        val credential= GoogleAuthProvider.getCredential(account.idToken,null)
+        firebaseAuth.signInWithCredential(credential).addOnCompleteListener {task->
+            if(task.isSuccessful) {
+             //   SavedPreference.setEmail(this,account.email.toString())
+             //   SavedPreference.setUsername(this,account.displayName.toString())
+                val intent = Intent(this, UserProfileActivity::class.java)
+                startActivity(intent)
+                finish()
+            }
+        }
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if(GoogleSignIn.getLastSignedInAccount(this)!=null){
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
     }
 
     private fun signInWithEmailAndPassword(email: String, password: String){
-        auth!!.signInWithEmailAndPassword(email, password)
+        firebaseAuth!!.signInWithEmailAndPassword(email, password)
             .addOnCompleteListener(){
                 if(it.isSuccessful){
                     Toast.makeText(this,"User Logged In Successfully", Toast.LENGTH_LONG).show()
@@ -73,7 +137,7 @@ class MainActivity : AppCompatActivity() {
             }
     }
 
-    private fun clearScreen(){
+    private fun clearEditText(){
         emailSignIn.setText("")
         passwordSignIn.setText("")
 
